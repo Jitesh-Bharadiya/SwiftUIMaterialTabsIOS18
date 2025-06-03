fa1efc0b10920f831f63371fc9514fde751221c2
Jitesh-Bharadiya <jitesh@humanshere.ai>
2025-06-03 12:20:30 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -16,9 +16,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftUIMaterialTabs"),
-        .testTarget(
-            name: "SwiftUIMaterialTabsTests",
-            dependencies: ["SwiftUIMaterialTabs"]),
-    ]
+            name: "SwiftUIMaterialTabs")
+    ],
+    swiftLanguageModes: [.v5]
 )
